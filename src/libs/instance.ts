3208bb033553724c298d9141e0b8f1f129765dfd
gracekim527 <lemonherb0323@naver.com>
2025-04-02 19:07:33 +0900

    :tada: Chore: 프로젝트 게시

---
+++ src/libs/instance.ts
@@ -0,0 +1,9 @@
+// src/lib/instance.ts
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+});
+
+export default instance;
