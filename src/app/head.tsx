862a2e1877557de3a2ba8bed111c1e113b8806cc
gracekim527 <lemonherb0323@naver.com>
2025-04-10 20:21:41 +0900

    :memo: Docs: 메타데이터 추가

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>SKHUni | 성공회대 IT 커뮤니티</title>
+      <meta property="og:title" content="성공회대 IT 커뮤니티, SKHUni" />
+      <meta
+        property="og:description"
+        content="성공회대 IT학생들과 네트워킹하고 커피챗을 신청해보세요!"
+      />
+      <meta property="og:image" content="https://skhuni.vercel.app/og-image.png" />
+      <meta property="og:url" content="https://skhuni.vercel.app" />
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   title: 'SKHUni | 성공회대 IT 커뮤니티',
   description: '성공회대 IT 학우들과 함께 성장하는 커뮤니티 플랫폼',
   openGraph: {
-    title: '스쿠니 | 성공회대 IT 커뮤니티',
+    title: 'SKHUni | 성공회대 IT 커뮤니티',
     description: '성공회대 IT 학우들과 함께 성장하는 커뮤니티 플랫폼',
     images: ['https://skhuni.vercel.app/og-image.png'],
     url: 'https://skhuni.vercel.app/',
